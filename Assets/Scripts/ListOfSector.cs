e8130b2b44cdf6990d442a870cec5f463e40a14a
kealu <ulaekbryant123@gmail.com>
2018-03-03 18:18:40 +0800

    listofcity

---
+++ Assets/Scripts/ListOfSector.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Minigame.Models;
+
+namespace Assets.Scripts.Minigame.Models
+{
+    public class ListOfCity
+    {
+        //create city model
+        public List<Sector> group1 = new List<Sector>()
+        {
+          new Sector{sectorNumber=1,conqueror=null},
+          new Sector{sectorNumber=3,conqueror=null},
+          new Sector{sectorNumber=11,conqueror=null},
+          new Sector{sectorNumber=19,conqueror=null},
+          new Sector{sectorNumber=37,conqueror=null},
+          new Sector{sectorNumber=55,conqueror=null}
+        };
+
+        public List<Sector> group2 = new List<Sector>()
+        {
+          new Sector{sectorNumber=4,conqueror=null},
+          new Sector{sectorNumber=12,conqueror=null},
+          new Sector{sectorNumber=20,conqueror=null},
+          new Sector{sectorNumber=38,conqueror=null},
+          new Sector{sectorNumber=56,conqueror=null},
+          new Sector{sectorNumber=88,conqueror=null}
+        };
+
+        public List<Sector> group3 = new List<Sector>()
+        {
+          new Sector{sectorNumber=21,conqueror=null},
+          new Sector{sectorNumber=39,conqueror=null},
+          new Sector{sectorNumber=22,conqueror=null},
+          new Sector{sectorNumber=40,conqueror=null},
+          new Sector{sectorNumber=72,conqueror=null}
+    
+        };
+
+        public List<Sector> group4 = new List<Sector>()
+        {
+          new Sector{sectorNumber=23,conqueror=null},
+          new Sector{sectorNumber=41,conqueror=null},
+          new Sector{sectorNumber=73,conqueror=null},
+          new Sector{sectorNumber=24,conqueror=null},
+          new Sector{sectorNumber=42,conqueror=null},
+          new Sector{sectorNumber=74,conqueror=null}
+
+        };
+
+        public List<Sector> group5 = new List<Sector>()
+        {
+          new Sector{sectorNumber=25,conqueror=null},
+          new Sector{sectorNumber=43,conqueror=null},
+          new Sector{sectorNumber=75,conqueror=null},
+          new Sector{sectorNumber=26,conqueror=null},
+          new Sector{sectorNumber=44,conqueror=null},
+          new Sector{sectorNumber=76,conqueror=null}
+        };
+
+        public List<Sector> group6 = new List<Sector>()
+        {
+          new Sector{sectorNumber=27,conqueror=null},
+          new Sector{sectorNumber=45,conqueror=null},
+          new Sector{sectorNumber=77,conqueror=null},
+          new Sector{sectorNumber=28,conqueror=null},
+          new Sector{sectorNumber=46,conqueror=null},
+          new Sector{sectorNumber=78,conqueror=null}
+        };
+
+        public List<Sector> group7 = new List<Sector>()
+        {
+          new Sector{sectorNumber=29,conqueror=null},
+          new Sector{sectorNumber=47,conqueror=null},
+          new Sector{sectorNumber=79,conqueror=null},
+          new Sector{sectorNumber=30,conqueror=null},
+          new Sector{sectorNumber=48,conqueror=null},
+          new Sector{sectorNumber=80,conqueror=null}
+        };
+
+        public List<Sector> group8 = new List<Sector>()
+        {
+          new Sector{sectorNumber=5,conqueror=null},
+          new Sector{sectorNumber=13,conqueror=null},
+          new Sector{sectorNumber=31,conqueror=null},
+          new Sector{sectorNumber=49,conqueror=null},
+          new Sector{sectorNumber=81,conqueror=null}
+        };
+
+        public List<Sector> group9 = new List<Sector>()
+        {
+          new Sector{sectorNumber=6,conqueror=null},
+          new Sector{sectorNumber=14,conqueror=null},
+          new Sector{sectorNumber=32,conqueror=null},
+          new Sector{sectorNumber=50,conqueror=null},
+          new Sector{sectorNumber=82,conqueror=null}
+        };
+
+        public List<Sector> group10 = new List<Sector>()
+        {
+          new Sector{sectorNumber=7,conqueror=null},
+          new Sector{sectorNumber=15,conqueror=null},
+          new Sector{sectorNumber=33,conqueror=null},
+          new Sector{sectorNumber=51,conqueror=null},
+          new Sector{sectorNumber=83,conqueror=null}
+        };
+
+        public List<Sector> group11 = new List<Sector>()
+        {
+          new Sector{sectorNumber=8,conqueror=null},
+          new Sector{sectorNumber=16,conqueror=null},
+          new Sector{sectorNumber=34,conqueror=null},
+          new Sector{sectorNumber=52,conqueror=null},
+          new Sector{sectorNumber=84,conqueror=null}
+        };
+
+        public List<Sector> group12 = new List<Sector>()
+        {
+          new Sector{sectorNumber=9,conqueror=null},
+          new Sector{sectorNumber=17,conqueror=null},
+          new Sector{sectorNumber=35,conqueror=null},
+          new Sector{sectorNumber=53,conqueror=null},
+          new Sector{sectorNumber=85,conqueror=null}
+        };
+
+        public List<Sector> group13 = new List<Sector>()
+        {
+          new Sector{sectorNumber=2,conqueror=null},
+          new Sector{sectorNumber=10,conqueror=null},
+          new Sector{sectorNumber=18,conqueror=null},
+          new Sector{sectorNumber=36,conqueror=null},
+          new Sector{sectorNumber=54,conqueror=null},
+          new Sector{sectorNumber=86,conqueror=null}
+        };
+
+        public List<Sector> group14 = new List<Sector>()
+        {
+          new Sector{sectorNumber=57,conqueror=null},
+          new Sector{sectorNumber=58,conqueror=null},
+          new Sector{sectorNumber=59,conqueror=null},
+          new Sector{sectorNumber=60,conqueror=null},
+          new Sector{sectorNumber=61,conqueror=null}
+        };
+
+        public List<Sector> group15 = new List<Sector>()
+        {
+          new Sector{sectorNumber=89,conqueror=null},
+          new Sector{sectorNumber=90,conqueror=null},
+          new Sector{sectorNumber=91,conqueror=null},
+          new Sector{sectorNumber=92,conqueror=null},
+          new Sector{sectorNumber=93,conqueror=null}
+        };
+
+        public List<Sector> group16 = new List<Sector>()
+        {
+          new Sector{sectorNumber=62,conqueror=null},
+          new Sector{sectorNumber=63,conqueror=null},
+          new Sector{sectorNumber=64,conqueror=null},
+          new Sector{sectorNumber=65,conqueror=null},
+          new Sector{sectorNumber=66,conqueror=null}
+        };
+
+        public List<Sector> group17 = new List<Sector>()
+        {
+          new Sector{sectorNumber=94,conqueror=null},
+          new Sector{sectorNumber=95,conqueror=null},
+          new Sector{sectorNumber=96,conqueror=null},
+          new Sector{sectorNumber=97,conqueror=null},
+          new Sector{sectorNumber=98,conqueror=null}
+        };
+
+        public List<Sector> group18 = new List<Sector>()
+        {
+          new Sector{sectorNumber=67,conqueror=null},
+          new Sector{sectorNumber=68,conqueror=null},
+          new Sector{sectorNumber=69,conqueror=null},
+          new Sector{sectorNumber=70,conqueror=null},
+          new Sector{sectorNumber=71,conqueror=null}
+        };
+
+        public List<Sector> group19 = new List<Sector>()
+        {
+          new Sector{sectorNumber=99,conqueror=null},
+          new Sector{sectorNumber=100,conqueror=null},
+          new Sector{sectorNumber=101,conqueror=null},
+          new Sector{sectorNumber=102,conqueror=null},
+          new Sector{sectorNumber=103,conqueror=null}
+        };
+    }
+}
---
+++ Assets/Scripts/Minigame/Models/Sector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Minigame.Models
+{
+    public class Sector
+    {
+        public int sectorNumber { get; set; }
+        public User conqueror = new User();
+    }
+}
