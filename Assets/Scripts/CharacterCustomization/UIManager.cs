ab24fdb932e4fbb08ae1e22d6d6d3fa8bafaea2d
kealurullamas <36137954+kealurullamas@users.noreply.github.com>
2018-07-15 19:34:13 +0800

    changes_in_character_customization

--- Assets/Scripts/CharacterCustomization/UIManager.cs
+++ Assets/Scripts/CharacterCustomization/UIManager.cs
@@ -9,19 +9,57 @@ public class UIManager : MonoBehaviour {
 	void Start () {
         featureManager = FindObjectOfType<FeatureManager>();
         
-        transform.Find("NavigationPanel").Find("FacePreviousButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(1); featureManager.PreviousChoice(); });
-        transform.Find("NavigationPanel").Find("FaceNextButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(1); featureManager.NextChoice(); });
-        transform.Find("NavigationPanel").Find("HairPreviousButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(2); featureManager.PreviousChoice(); });
-        transform.Find("NavigationPanel").Find("HairNextButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(2); featureManager.NextChoice(); });
-        transform.Find("NavigationPanel").Find("EyesPreviousButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(3); featureManager.PreviousChoice(); ; });
-        transform.Find("NavigationPanel").Find("EyesNextButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(3); featureManager.NextChoice(); });
-        transform.Find("NavigationPanel").Find("NosePreviousButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(4); featureManager.PreviousChoice(); });
-        transform.Find("NavigationPanel").Find("NoseNextButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(4); featureManager.NextChoice(); });
-        transform.Find("NavigationPanel").Find("MouthPreviousButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(5); featureManager.PreviousChoice(); });
-        transform.Find("NavigationPanel").Find("MouthNextButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.SetCurrent(5); featureManager.NextChoice(); });
+        transform.Find("NavigationPanel").Find("FacePreviousButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.SetCurrent(1);
+            transform.Find("NavigationPanel").Find("HairNum").GetComponent<Text>().text = featureManager.PreviousChoiceTest().ToString() + "/20";
+        });
+        transform.Find("NavigationPanel").Find("FaceNextButton").GetComponent<Button>().onClick.AddListener(() => { 
+            featureManager.SetCurrent(1); 
+            transform.Find("NavigationPanel").Find("HairNum").GetComponent<Text>().text = featureManager.NextChoiceTest().ToString() + "/20"; 
+        });
+        transform.Find("NavigationPanel").Find("HairPreviousButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.SetCurrent(2);
+            transform.Find("NavigationPanel").Find("BrowNum").GetComponent<Text>().text = featureManager.PreviousChoiceTest().ToString() + "/20";
+        });
+        transform.Find("NavigationPanel").Find("HairNextButton").GetComponent<Button>().onClick.AddListener(() => { 
+            featureManager.SetCurrent(2);
+            transform.Find("NavigationPanel").Find("BrowNum").GetComponent<Text>().text = featureManager.NextChoiceTest().ToString() + "/20"; 
+        });
+        transform.Find("NavigationPanel").Find("EyesPreviousButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.SetCurrent(3);
+            transform.Find("NavigationPanel").Find("EyeNum").GetComponent<Text>().text = featureManager.PreviousChoiceTest().ToString() + "/25";
+        });
+        transform.Find("NavigationPanel").Find("EyesNextButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.SetCurrent(3);
+            transform.Find("NavigationPanel").Find("EyeNum").GetComponent<Text>().text = featureManager.NextChoiceTest().ToString() + "/25";
+        });
+        transform.Find("NavigationPanel").Find("NosePreviousButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.SetCurrent(4);
+            transform.Find("NavigationPanel").Find("NoseNum").GetComponent<Text>().text = featureManager.PreviousChoiceTest().ToString() + "/5";
+        });
+        transform.Find("NavigationPanel").Find("NoseNextButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.SetCurrent(4);
+            transform.Find("NavigationPanel").Find("NoseNum").GetComponent<Text>().text = featureManager.NextChoiceTest().ToString() + "/5"; 
+        });
+        transform.Find("NavigationPanel").Find("MouthPreviousButton").GetComponent<Button>().onClick.AddListener(() => { 
+            featureManager.SetCurrent(5);
+            transform.Find("NavigationPanel").Find("MouthNum").GetComponent<Text>().text = featureManager.PreviousChoiceTest().ToString() + "/5";
+        });
+        transform.Find("NavigationPanel").Find("MouthNextButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.SetCurrent(5);
+            transform.Find("NavigationPanel").Find("MouthNum").GetComponent<Text>().text = featureManager.NextChoiceTest().ToString() + "/5";
+        });
 
-        transform.Find("NavigationPanel").Find("MaleButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.LoadMaleFeatures(); });
-        transform.Find("NavigationPanel").Find("FemaleButton").GetComponent<Button>().onClick.AddListener(() => { featureManager.LoadFemaleFeatures(); });
+        transform.Find("NavigationPanel").Find("MaleButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.LoadMaleFeatures();
+            transform.Find("NavigationPanel").Find("FemaleButton").GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            transform.Find("NavigationPanel").Find("MaleButton").GetComponent<Image>().color = new Color32(245, 133, 23, 255);
+        });
+        transform.Find("NavigationPanel").Find("FemaleButton").GetComponent<Button>().onClick.AddListener(() => {
+            featureManager.LoadFemaleFeatures();
+            transform.Find("NavigationPanel").Find("MaleButton").GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            transform.Find("NavigationPanel").Find("FemaleButton").GetComponent<Image>().color = new Color32(245, 133, 23, 255);
+        });
 	}
 	
 
