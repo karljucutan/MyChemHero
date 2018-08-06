8ae0260e746af3d3e197938e18fb26c0199886ba
kealurullamas <36137954+kealurullamas@users.noreply.github.com>
2018-08-06 22:58:44 +0800

    change

--- Assets/Scripts/Configuration.cs
+++ Assets/Scripts/Configuration.cs
@@ -9,13 +9,13 @@ namespace Assets.Scripts
     public class Configuration
     {
 
-        //public const string BASE_ADDRESS = "http://www.mychemhero.com/";
-        public const string BASE_ADDRESS = "http://localhost/ChemHero/";
+        public const string BASE_ADDRESS = "http://www.mychemhero.com/";
+        //public const string BASE_ADDRESS = "http://localhost/ChemHero/";
         //public const string BASE_ADDRESS = "http://172.16.17.100/ChemHero/";
 
-        public static string VideoURL = Application.streamingAssetsPath + "/MCH_OP.mp4"; //"file://C:/Users/Kenneth/Documents/MCH_OP.mp4";
-        //public static string VideoURL = "http://mychemhero.com/assets/MCH_OP.mp4";
+        //public static string VideoURL = Application.streamingAssetsPath + "/MCH_OP.mp4"; //"file://C:/Users/Kenneth/Documents/MCH_OP.mp4";
+        public static string VideoURL = "http://www.mychemhero.com/assets/MCH_OP.mp4";
         //public const string NAME = "kealu";
-        public const int ID = 2;
+        //public const int ID = 3;
     }
 }
